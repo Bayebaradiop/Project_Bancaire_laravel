0011e286c649b76166cdc3f7eb26fa6e653a15e8
Bayebaradiop <bayebara2000@gmail.com>
2025-10-23 18:58:07 +0000

    fix(docker): clear Laravel caches before rebuilding in production

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -32,6 +32,9 @@ fi
 
 # Optimiser l'application
 echo "Optimizing application..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
